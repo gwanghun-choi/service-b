286507f7b99d9708b10dd7d15a577c8439d9dfea
gwanghun-choi <choi_huni@naver.com>
2025-12-03 15:09:36 +0900

    init: service-b

---
+++ src/main/java/com/example/serviceb/controller/StatusController.java
@@ -0,0 +1,13 @@
+package com.example.serviceb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/b/status")
+    public String status() {
+        return "B OK";
+    }
+}
